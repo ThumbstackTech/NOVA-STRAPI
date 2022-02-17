28cf023e4b80889118fccee02e725fac8ab38a8b
SURAJ <republicofgamers.srg@gmail.com>
2022-02-17 22:25:06 +0530

    before pull

--- config/database.js
+++ config/database.js
@@ -2,9 +2,9 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'mysql',
     connection: {
-      host: env('DATABASE_HOST', '3.111.24.61'),
+      host: env('DATABASE_HOST', '144.126.252.122'),
       port: env.int('DATABASE_PORT', 3306),
-      database: env('DATABASE_NAME', 'nova'),
+      database: env('DATABASE_NAME', 'test'),
       user: env('DATABASE_USERNAME', 'iifl'),
       password: env('DATABASE_PASSWORD', 'Ritesh@123'),
       ssl: env.bool('DATABASE_SSL', false),
