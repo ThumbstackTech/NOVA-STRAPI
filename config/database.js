731ce2da19222919f3d926a2cf5484f859a40e14
nitin <gangwarnitin111@gmail.com>
2022-02-17 13:54:37 +0530

    a lot of data added

--- config/database.js
+++ config/database.js
@@ -1,13 +1,13 @@
 module.exports = ({ env }) => ({
   connection: {
-    client: 'mysql',
+    client: "mysql",
     connection: {
-      host: env('DATABASE_HOST', '3.111.24.61'),
-      port: env.int('DATABASE_PORT', 3306),
-      database: env('DATABASE_NAME', 'nova'),
-      user: env('DATABASE_USERNAME', 'iifl'),
-      password: env('DATABASE_PASSWORD', 'Ritesh@123'),
-      ssl: env.bool('DATABASE_SSL', false),
+      host: env("DATABASE_HOST", "3.111.24.61"),
+      port: env.int("DATABASE_PORT", 3306),
+      database: env("DATABASE_NAME", "test"),
+      user: env("DATABASE_USERNAME", "iifl"),
+      password: env("DATABASE_PASSWORD", "Ritesh@123"),
+      ssl: env.bool("DATABASE_SSL", false),
     },
   },
 });
