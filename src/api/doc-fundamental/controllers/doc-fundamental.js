d40496604f05fff7ea422779ac114927baff4f2e
nitin <gangwarnitin111@gmail.com>
2022-02-22 09:00:49 +0530

    change

--- src/api/doc-fundamental/controllers/doc-fundamental.js
+++ src/api/doc-fundamental/controllers/doc-fundamental.js
@@ -148,6 +148,121 @@ module.exports = createCoreController(
               },
             },
           },
+          api_kits_cards: {
+            fields: [
+              "title",
+              "subTitle",
+              "cardTag",
+              "subTag",
+              "desc",
+              "filterTag",
+            ],
+            populate: {
+              avatar: {
+                fields: ["url"],
+              },
+              descImg: {
+                fields: ["url"],
+              },
+              bgSvg: {
+                fields: ["url"],
+              },
+              topRightCornerImg: {
+                fields: ["url"],
+              },
+              points: {
+                fields: ["title", "tag", "sortDesc"],
+                populate: {
+                  bg: {
+                    fields: ["url"],
+                  },
+                  bgSvg: {
+                    fields: ["url"],
+                  },
+                  icon: {
+                    fields: ["url"],
+                  },
+                },
+              },
+              api_info: {
+                fields: ["title", "tag", "titleTag", "api", "apiCallsAvg"],
+                populate: {
+                  icon: {
+                    fields: ["url"],
+                  },
+                },
+              },
+              download_info: {
+                fields: ["title", "tag", "downloadTag", "downloadsCount"],
+                populate: {
+                  icon: {
+                    fields: ["url"],
+                  },
+                },
+              },
+              investment_info: {
+                fields: [
+                  "title",
+                  "tag",
+                  "investmentTag",
+                  "investmentAmount",
+                ],
+                populate: {
+                  icon: {
+                    fields: ["url"],
+                  },
+                },
+              },
+              rating_info: {
+                fields: [
+                  "title",
+                  "tag",
+                  "ratingTag",
+                  "ratingCount",
+                  "rating",
+                ],
+                populate: {
+                  icon: {
+                    fields: ["url"],
+                  },
+                },
+              },
+              return_info: { fields: ["title", "tag", "percentageReturn"] },
+              links: {
+                fields: ["title", "href"],
+                populate: {
+                  icon: { fields: ["url"] },
+                },
+              },
+              link_collections: {
+                fields: ["title"],
+                populate: {
+                  links: {
+                    fields: ["title", "href", "tag"],
+                    populate: {
+                      icon: { fields: ["url"] },
+                    },
+                  }
+                }
+              },
+              response_info: {
+                fields: ["title", "tag"],
+                populate: {
+                  icon: {
+                    fields: ["url"],
+                  },
+                },
+              },
+              success_info: {
+                fields: ["title", "tag", "successRate"],
+                populate: {
+                  icon: {
+                    fields: ["url"],
+                  },
+                },
+              },
+            },
+          },
           points: {
             fields: ["title", "tag", "sortDesc", "description"],
             populate: {
